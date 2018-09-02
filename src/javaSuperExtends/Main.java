2aff4a20517f7f82aecded8e75f6c2a4189a7621
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-09-03 06:12:50 +0900

    final int = not change error

---
+++ src/javaSuperExtends/Main.java
@@ -0,0 +1,5 @@
+package javaSuperExtends;
+
+public class Main {
+
+}
---
+++ src/javaSuperExtends/Monster.java
@@ -0,0 +1,10 @@
+package javaSuperExtends;
+
+public class Monster {
+	final int HP = 10;
+	final int MP = 100;
+	String name;
+	Monster(String name,int HP ,int MP){
+		this.HP = HP;
+	}
+}
